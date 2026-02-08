45667bf59ad71f432e0a57db110c59230e810277
cooooing <75006169+Cooooing@users.noreply.github.com>
2026-02-08 13:32:48 +0800

    refactor: clean up code

--- common/pkg/constant/service.go
+++ common/pkg/constant/service.go
@@ -3,13 +3,13 @@ package constant
 type ServiceName string
 
 const (
-	GatewayServiceName ServiceName = "gateway-service"
-	UserServiceName    ServiceName = "user-service"
-	ContentServiceName ServiceName = "content-service"
-	NotifyServiceName  ServiceName = "notify-service"
-	IMServiceName      ServiceName = "im-service"
-	SignalServiceName  ServiceName = "signal-service"
-	InfraServiceName   ServiceName = "infra-service"
+	GatewayServiceName ServiceName = "gateway"
+	UserServiceName    ServiceName = "user"
+	ContentServiceName ServiceName = "content"
+	NotifyServiceName  ServiceName = "notify"
+	IMServiceName      ServiceName = "im"
+	SignalServiceName  ServiceName = "signal"
+	InfraServiceName   ServiceName = "infra"
 )
 
 func (s ServiceName) String() string {

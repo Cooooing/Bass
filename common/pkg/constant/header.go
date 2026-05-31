6ee982e8e286f5cfa2e75af96a43dcb91b4330b2
cooooing <75006169+Cooooing@users.noreply.github.com>
2026-05-31 21:33:32 +0800

    refactor

---
+++ common/pkg/constant/header.go
@@ -0,0 +1,16 @@
+package constant
+
+// 请求元信息头名称定义。
+const (
+	HeaderUserAgent      = "User-Agent"
+	HeaderDeviceID       = "X-Device-ID"
+	HeaderPlatform       = "X-Platform"
+	HeaderRequestID      = "X-Request-ID"
+	HeaderTraceID        = "X-Trace-ID"
+	HeaderForwardedFor   = "X-Forwarded-For"
+	HeaderRealIP         = "X-Real-IP"
+	HeaderClientIP       = "X-Client-IP"
+	HeaderTimestamp      = "X-Timestamp"   // 时间戳，防止过期请求
+	HeaderNonce          = "X-Nonce"       // 随机数，防止重放攻击
+	HeaderAuthentication = "Authorization" // 令牌请求头名称
+)
---
+++ common/pkg/util/server/request.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"common/pkg/constant"
+	"common/pkg/model"
+	"common/pkg/util"
+	"context"
+	"strings"
+)
+
+// ClientIP 返回请求的客户端 IP，优先使用上下文中已解析的 IP 信息。
+func ClientIP(ctx context.Context) string {
+	if ipInfo, ok := util.GetContextValue[*model.IpInfo](ctx, constant.CtxIpInfo); ok && ipInfo != nil {
+		if ip := strings.TrimSpace(ipInfo.Ip); ip != "" {
+			return ip
+		}
+	}
+	for _, header := range [...]string{constant.HeaderForwardedFor, constant.HeaderRealIP, constant.HeaderClientIP} {
+		for _, item := range strings.Split(GetHeader(ctx, header), ",") {
+			if ip := strings.TrimSpace(item); ip != "" {
+				return ip
+			}
+		}
+	}
+	return ""
+}
